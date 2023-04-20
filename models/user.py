aea85ce0df961d5230a48861be57e2ab3015fa09
factism001 <factism001@gmail.com>
2023-04-20 11:09:34 +0100

    update

--- models/user.py
+++ models/user.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This is the user class."""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Column, Integer, String
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-        User class for the user
-    """
-    if (storage_engine == 'db'):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    reviews = relationship('Review', backref='user',
+                           cascade='delete')
+    places = relationship('Place', backref='user',
+                          cascade='delete')
