46a21c04ad8ae0ebe8a6fb5e6add1beccb0e71db
factism001 <factism001@gmail.com>
2023-05-03 15:46:02 +0100

    more solutions

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-"""web server distribution"""
+"""web server distribution
+    """
 from fabric.api import *
+import tarfile
 import os.path
+import re
+from datetime import datetime
 
 env.user = 'ubuntu'
 env.hosts = ["35.174.209.230", "54.157.184.171"]
 env.key_filename = "~/id_rsa"
+
+
+def do_pack():
+    """distributes an archive to your web servers
+    """
+    target = local("mkdir -p ./versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("./versions/web_static_{}.tgz".format(opt))
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -27,3 +44,12 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+
+def deploy():
+    """distributes an archive to your web servers"""
+    path = do_pack()
+    if path is None:
+        return False
+    f = do_deploy(path)
+    return f
